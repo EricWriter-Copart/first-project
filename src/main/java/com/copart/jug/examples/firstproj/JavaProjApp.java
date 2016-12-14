a367a62d5ee304b43fd3528d1209453c716d3956
Eric <eric.writer@copart.com>
2016-12-14 15:46:29 -0800

    Created Main Class

---
+++ src/main/java/com/copart/jug/examples/firstproj/JavaProjApp.java
@@ -0,0 +1,12 @@
+package com.copart.jug.examples.firstproj;
+
+public class JavaProjApp
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Print this argument: " + args[0]);
+		System.out.println("Print second argument: " + args[1]);
+	}
+
+}
